8d0146e4f5a1cf9b049b97de0189f90096a0f111
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2016-01-05 10:27:07 +0000

    copy previous term page

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ python current.py
 python htmlgen.py
 mkdir -p ../public_html/NASeminar
 cp naseminar.html ../public_html/NASeminar
+cp naseminar2015sem1.html ../public_html/NASeminar
 cp -r Abstracts ../public_html/NASeminar/
