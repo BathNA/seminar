2b94cae3a42ff5063a2cb9060452eeff83ba8f9b
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2016-02-05 10:36:58 +0000

    try adding htaccess to prevent caching

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ python current.py
 python htmlgen.py
 mkdir -p ../public_html/NASeminar
 cp *.html ../public_html/NASeminar
+cp .htaccess ../public_html/NASeminar
 cp -r Abstracts ../public_html/NASeminar/
