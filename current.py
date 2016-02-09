4c7a2d5b41e75fd4775f11441153d9caffa50e4a
Eike Mueller <eike.h.mueller@gmail.com>
2016-02-09 08:08:34 +0000

    Added Christian Litterer's talk

--- current.py
+++ current.py
@@ -19,7 +19,7 @@ seminars.append(["11 Mar 2016", "Sergey Dolgov", "Bath", "Tensor product approac
 seminars.append(["18 Mar 2016", "Daniel Ruprecht", "Leeds", "Spectral deferred corrections with fast-wave slow-wave splitting (exact title TBC)", "https://www.engineering.leeds.ac.uk/people/mechanical/staff/d.ruprecht", None, None])
 seminars.append(["08 Apr 2016", "Matthew Thomas", "Bath", "TBC", "", None, None])
 seminars.append(["15 Apr 2016", "Patrick Kuerschner", "MPI Magdeburg", "TBC", "http://www2.mpi-magdeburg.mpg.de/mpcsc/kuerschner/", None, None])
-seminars.append(["22 Apr 2016", "TBC", "", "", "", None, None])
+seminars.append(["22 Apr 2016", "Christian Litterer", "York", "TBC", "http://maths.york.ac.uk/www/cl1566", None, None])
 seminars.append(["29 Apr 2016", "Alison Ramage", "Strathclyde", "A multilevel preconditioner for data assimilation with 4D-Var", "http://www.strath.ac.uk/staff/ramagealisondr/", "ramage.txt", None])
 
 links = []
