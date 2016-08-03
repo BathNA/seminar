4d773b17f39e78b52a535ceaf42edda0c2e03067
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2016-08-03 10:52:39 +0100

    add Joab title

--- current.py
+++ current.py
@@ -17,7 +17,7 @@ seminars.append(["04 Nov 2016", "", "", "", "", "", None])
 seminars.append(["11 Nov 2016", "Sébastien Loisel", "Heriot-Watt", "", "http://www.macs.hw.ac.uk/~sl398/", "", None])
 seminars.append(["18 Nov 2016", "", "", "", "", "", None])
 seminars.append(["25 Nov 2016", "", "", "", "", "", None])
-seminars.append(["02 Dec 2016", "Joab Winkler", "Sheffield", "", "http://staffwww.dcs.shef.ac.uk/people/J.Winkler/", "", None])
+seminars.append(["02 Dec 2016", "Joab Winkler", "Sheffield", "Polynomial computations for blind image deconvolution", "http://staffwww.dcs.shef.ac.uk/people/J.Winkler/", "", None])
 seminars.append(["09 Dec 2016", "", "", "", "", "", None])
 seminars.append(["16 Dec 2016", "", "", "", "", "", None])
 
