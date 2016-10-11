29307e5d233c71f0c3491d960047039fe1ecfda6
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2016-10-11 15:18:04 +0100

    Add Abdul-Lateef talk

--- current.py
+++ current.py
@@ -11,7 +11,7 @@ seminars = []
 
 seminars.append(["07 Oct 2016", "James Hook", "Bath", "Applications of tropical mathematics in numerical linear algebra", "http://www.bath.ac.uk/imi/people/index.html", "hook.txt", None])
 seminars.append(["14 Oct 2016", "Silvia Gazzola", "Bath", "New challenges in the numerical solution of large-scale inverse problems", "http://people.bath.ac.uk/sg968/", "gazzola.txt", None])
-seminars.append(["21 Oct 2016", "Abdul-Lateef Haji-Ali", "Oxford", "", "https://www.maths.ox.ac.uk/people/abdullateef.hajiali", "", None])
+seminars.append(["21 Oct 2016", "Abdul-Lateef Haji-Ali", "Oxford", "Multi-index methods for quadrature", "https://www.maths.ox.ac.uk/people/abdullateef.hajiali", "hajiali.txt", None])
 seminars.append(["28 Oct 2016", "Lei Zhang", "Shanghai Jiao Tong", "Solving multi-scale PDEs: from numerical homogenization to fast solvers", "http://ins.sjtu.edu.cn/faculty/zhanglei", "", None])
 seminars.append(["04 Nov 2016", "Alastair Gregory", "Imperial", "A multilevel Monte Carlo approach to ensemble transform particle filtering", "http://www.imperial.ac.uk/people/a.gregory14", "gregory.txt", None])
 seminars.append(["11 Nov 2016", "Sébastien Loisel", "Heriot-Watt", "Optimized Schwarz method for heterogeneous problems", "http://www.macs.hw.ac.uk/~sl398/", "", None])
