7a4ddd51be64f89533f3b1f09fb83a71acc2eb7e
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2016-11-17 13:34:16 +0000

    add Rob Scheichl abstract

--- current.py
+++ current.py
@@ -15,7 +15,7 @@ seminars.append(["21 Oct 2016", "Abdul-Lateef Haji-Ali", "Oxford", "Multi-index 
 seminars.append(["28 Oct 2016", "Lei Zhang", "Shanghai Jiao Tong", "Solving multi-scale PDEs: from numerical homogenization to fast solvers", "http://ins.sjtu.edu.cn/faculty/zhanglei", "zhang.txt", None])
 seminars.append(["04 Nov 2016", "Alastair Gregory", "Imperial", "A multilevel Monte Carlo approach to ensemble transform particle filtering", "http://www.imperial.ac.uk/people/a.gregory14", "gregory.txt", None])
 seminars.append(["11 Nov 2016", "Sébastien Loisel", "Heriot-Watt", "Optimized Schwarz methods for heterogeneous problems", "http://www.macs.hw.ac.uk/~sl398/", "loisel.txt", None])
-seminars.append(["18 Nov 2016", "Rob Scheichl", "Bath", "Multilevel Subset Simulation to Predict Rare Events", "http://www.bath.ac.uk/math-sci/contacts/academics/rob-scheichl/", "", None])
+seminars.append(["18 Nov 2016", "Rob Scheichl", "Bath", "Multilevel Subset Simulation to Predict Rare Events", "http://www.bath.ac.uk/math-sci/contacts/academics/rob-scheichl/", "scheichl.txt", None])
 seminars.append(["25 Nov 2016", "Claudia Schillings", "HU Berlin", "Scaling Limits in Computational Bayesian Inversion", "https://www2.mathematik.hu-berlin.de/~schillcl/", "schillings.txt", None])
 seminars.append(["02 Dec 2016", "Joab Winkler", "Sheffield", "Image Processing by Polynomial Computations", "http://staffwww.dcs.shef.ac.uk/people/J.Winkler/", "winkler.txt", None])
 seminars.append(["09 Dec 2016", "Zhiwen Zhang", "Hong Kong", "", "http://hkumath.hku.hk/MathWWW/people.php?faculty.zhangzw", "", None])
