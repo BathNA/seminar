c7765b617535e6940c166298b93eff30ff5f9d33
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2016-12-19 14:37:47 +0000

    blank for semester 2

--- current.py
+++ current.py
@@ -4,24 +4,25 @@
 import pickle
 
 meta = []
-meta.append("1st Semester 2016/2017")
+meta.append("2nd Semester 2016/2017")
 
 seminars = []
 # Fields: date, speaker, institution, title, homepage URL, abstract link, misc
 
-seminars.append(["07 Oct 2016", "James Hook", "Bath", "Applications of tropical mathematics in numerical linear algebra", "http://www.bath.ac.uk/imi/people/index.html", "hook.txt", None])
-seminars.append(["14 Oct 2016", "Silvia Gazzola", "Bath", "New challenges in the numerical solution of large-scale inverse problems", "http://people.bath.ac.uk/sg968/", "gazzola.txt", None])
-seminars.append(["21 Oct 2016", "Abdul-Lateef Haji-Ali", "Oxford", "Multi-index methods for quadrature", "https://www.maths.ox.ac.uk/people/abdullateef.hajiali", "hajiali.txt", None])
-seminars.append(["28 Oct 2016", "Lei Zhang", "Shanghai Jiao Tong", "Solving multi-scale PDEs: from numerical homogenization to fast solvers", "http://ins.sjtu.edu.cn/faculty/zhanglei", "zhang.txt", None])
-seminars.append(["04 Nov 2016", "Alastair Gregory", "Imperial", "A multilevel Monte Carlo approach to ensemble transform particle filtering", "http://www.imperial.ac.uk/people/a.gregory14", "gregory.txt", None])
-seminars.append(["11 Nov 2016", "Sébastien Loisel", "Heriot-Watt", "Optimized Schwarz methods for heterogeneous problems", "http://www.macs.hw.ac.uk/~sl398/", "loisel.txt", None])
-seminars.append(["18 Nov 2016", "Rob Scheichl", "Bath", "Multilevel subset simulation to predict rare events", "http://www.bath.ac.uk/math-sci/contacts/academics/rob-scheichl/", "scheichl.txt", None])
-seminars.append(["25 Nov 2016", "Claudia Schillings", "HU Berlin", "Scaling limits in computational Bayesian inversion", "https://www2.mathematik.hu-berlin.de/~schillcl/", "schillings.txt", None])
-seminars.append(["02 Dec 2016", "Joab Winkler", "Sheffield", "Image processing by polynomial computations", "http://staffwww.dcs.shef.ac.uk/people/J.Winkler/", "winkler.txt", None])
-seminars.append(["09 Dec 2016", "Zhiwen Zhang", "Hong Kong", "Multiscale tailored finite point method for second order elliptic equations with rough or highly-oscillatory coefficients.", "http://hkumath.hku.hk/MathWWW/people.php?faculty.zhangzw", "zhang2.txt", None])
-seminars.append(["16 Dec 2016", "Elizabeth Arter", "Bath", "New low-rank results for a Green’s function of interest in preconditioning Helmholtz solvers", "", "arter.txt", None])
+seminars.append(["10 Feb 2017", "", "", "", "", "", None])
+seminars.append(["17 Feb 2017", "", "", "", "", "", None])
+seminars.append(["24 Feb 2017", "", "", "", "", "", None])
+seminars.append(["3 Mar 2017", "", "", "", "", "", None])
+seminars.append(["10 Mar 2017", "", "", "", "", "", None])
+seminars.append(["17 Mar 2017", "", "", "", "", "", None])
+seminars.append(["24 Mar 2017", "", "", "", "", "", None])
+seminars.append(["31 Mar 2017", "", "", "", "", "", None])
+seminars.append(["7 Apr 2017", "", "", "", "", "", None])
+seminars.append(["28 Apr 2017", "", "", "", "", "", None])
+seminars.append(["5 May 2017", "", "", "", "", "", None])
 
 links = []
+links.append(["2016/17 1st semester", "Archive/naseminar2016sem1.html"])
 links.append(["2015/16 2nd semester", "Archive/naseminar2015sem2.html"])
 links.append(["2015/16 1st semester", "Archive/naseminar2015sem1.html"])
 links.append(["2014/15 2nd semester", "http://people.bath.ac.uk/em459/NASeminar/naseminar2014sem2.html"])
