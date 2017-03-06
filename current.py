4377b6b49bbb1fd84c600a621a6fca3c182aa7d7
Andrew McRae <a.t.t.mcrae@bath.ac.uk>
2017-03-06 15:09:37 +0000

    add Can Evren Yarman talk

--- current.py
+++ current.py
@@ -16,7 +16,7 @@ seminars.append(["3 Mar 2017", "Tony Kennedy", "ATI/Edinburgh", "Hybrid Monte Ca
 seminars.append(["10 Mar 2017", "Marta Betcke", "UCL", "Dynamical photoacoustic imaging", "http://www0.cs.ucl.ac.uk/people/M.Betcke.html", "betcke.txt", None])
 seminars.append(["17 Mar 2017", "Ander Biguri and Manasavee Lohvithee", "Bath", "GPU-accelerated iterative algorithms in Cone Beam Computerized Tomography", "", "biguri.txt", None])
 seminars.append(["24 Mar 2017", "Anders Hansen", "Cambridge", "", "http://www.damtp.cam.ac.uk/people/a.c.hansen/", "", None])
-seminars.append(["31 Mar 2017", "", "", "", "", "", None])
+seminars.append(["31 Mar 2017", "Can Evren Yarman", "Schlumberger", "Generalization of Padé approximation from rational functions to arbitrary analytic functions", "", "yarman.txt", None])
 seminars.append(["7 Apr 2017", "Oliver Dorn", "Manchester", "", "http://www.maths.manchester.ac.uk/people/staff/profile/?ea=Oliver.Dorn", "", None])
 seminars.append(["28 Apr 2017", "Aretha Teckentrup", "Edinburgh", "", "http://www.maths.ed.ac.uk/school-of-mathematics/people?person=544", "", None])
 seminars.append(["5 May 2017", "", "", "", "", "", None])
